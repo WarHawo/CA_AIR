413c420e99bdd84405d399f64bcde465788c9885
Replit user <>
2023-04-27 15:32:14 +0000

    modification nom fonction

--- air02.js
+++ air02.js
@@ -1,9 +1,13 @@
 /* programme qui transforme un tableau de chaînes de caractères en une seule chaîne de caractères. Espacés d’un séparateur donné en dernier argument au programme.*/
 
-function my_str_concat(str, sep) {
+function myStrConcat(str, sep) {
   let newStr = "";
-  if(!str || !sep)
-    return "error";
+  
+  if (!str || !sep) {
+    console.error("erreur");
+    process.exit(1);
+  }
+
   for (let i = 0; i < str.length - 1; i++){
     newStr += str[i];
     if (i !== str.length - 2)
@@ -15,4 +19,4 @@ function my_str_concat(str, sep) {
 str = process.argv.slice(2);
 sep = str[str.length - 1];
 
-console.log(my_str_concat(str, sep));
+console.log(myStrConcat(str, sep));
