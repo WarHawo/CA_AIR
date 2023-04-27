9def9603f5ce18622b03bd1cc97449cd84457df0
Replit user <>
2023-04-27 15:27:02 +0000

    modifications erreur et nom fonction

--- air01.js
+++ air01.js
@@ -1,56 +1,12 @@
 /* programme qui découpe une chaîne de caractères en tableau en fonction du séparateur donné en 2e argument.*/
 
-function my_split(string, sep) {
-  let tab = [];
-  let str = "";
-  let nSep = 0;
-  let j = 0;
-  
-  if (!string || !sep || sep.length > 1)
-      return "error";
-  
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === sep)
-      nSep++;
+function mySplitStr(string, sep) {
+  if (!string || !sep) {
+    console.error("erreur");
+    process.exit(1);
   }
-  
-  for (let i = 0; i <= nSep; i++) {
-    while (string[j] !== sep && j < string.length) {
-      str += string [j];
-      j++;
-    }
-    tab[i] = str;
-    str = "";
-    j++;
-  }
-  return tab;
-}
-
-function my_split_str(string, sep) {
-  let newTab = [];
-  let nSep = 0;
-  let str = "";
-  let j = 0;
-  
-  let tab = my_split(string, " ");
-  for (let i = 0; i < tab.length; i++) {
-    if (tab[i] === sep)
-      nSep++;
-  }
-  for (let i = 0; i <= nSep; i++) {
-    while (tab[j] !== sep && j < tab.length) {
-      str += tab[j] + " ";
-      j++;
-    }
-    newTab[i] = str;
-    str = "";
-    j++;
-  }
-  return newTab;
-}
-
-function my_split_fct(string, sep) {
-  let newTab = my_split(string.replace(sep, "*"), "*");
+  //let newTab = mySplit(string.replaceAll(sep, "*"), "*");
+  let newTab = string.replaceAll(sep, "*").split("*");
   return newTab;
 }
 
@@ -63,4 +19,4 @@ function displayTab(tab){
 
 string = process.argv[2];
 sep = process.argv[3];
-displayTab(my_split_fct(string, sep));
+displayTab(mySplitStr(string, sep));
