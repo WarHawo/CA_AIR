e5c5b2126be5e77a7a5277e79e6939f54f96e45e
War <warhawo@gmail.com>
2023-04-29 09:33:51 +0200

    ex04 finit

---
+++ air04.js
@@ -0,0 +1,20 @@
+/*programme qui affiche une chaîne de caractères en évitant les caractères identiques adjacents.*/
+
+function oneCharOnly(args)
+{
+    if (args.length !== 1) {
+        console.error("erreur : nombre d'argument incorrecte");
+        process.exit(1);
+    }
+
+    let newStr = args[0][0];
+    for (let i = 1; i < args[0].length ; i++) {
+        if (args[0][i] !== args[0][i - 1]) {
+            newStr += args[0][i];
+        }
+    }
+    return newStr;
+}
+
+args = process.argv.slice(2);
+console.log(oneCharOnly(args));
